956a4ff5f909a9ec5113320737f6ae85b4b5633a
Adi Li <ctlihk@live.hk>
2015-11-21 21:35:51 +0800

    Initial Commit

---
+++ BigQuote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BigQuote
+//
+//  Created by Adi Li on 21/11/2015.
+//  Copyright © 2015 Adi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BigQuoteTests/BigQuoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  BigQuoteTests.swift
+//  BigQuoteTests
+//
+//  Created by Adi Li on 21/11/2015.
+//  Copyright © 2015 Adi Li. All rights reserved.
+//
+
+import XCTest
+@testable import BigQuote
+
+class BigQuoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
